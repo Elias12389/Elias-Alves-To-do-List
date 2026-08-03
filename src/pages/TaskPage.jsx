92f88258d14ad5375533c06da6c354a5f585ae14
Elias12389 <eliasalveees@gmail.com>
2026-08-03 00:22:49 -0300

    Navegação concluída

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -1,16 +1,37 @@
 //Forma como importamos
-import { useSearchParams } from "react-router-dom";
+import { ChevronLeftIcon } from "lucide-react";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 function TaskPage() {
   //O primeiro indice podendno conter qualquer nome
   const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
   //método get para justamente pegar pela url
   const title = searchParams.get("title");
   const description = searchParams.get("description");
   return (
-    <div className="h-screen w-screen bg-slate-500 p-6">
-      <h1>{title}</h1>
-      <p>{description}</p>
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px] space-y-4">
+        {/*classe pai*/}
+        <div className="flex justify-center relative">
+          {/*classe filho*/}
+          <button
+            onClick={() => navigate(-1)}
+            className="absolute left-0 top-0 bottom-0 text-slate-100"
+          >
+            <ChevronLeftIcon />
+          </button>
+          <h1 className="text-3xl text-slate-100 font-bold text-center my-2">
+            Detalhes da Tarefa
+          </h1>
+        </div>
+
+        <div className="bg-slate-200 p-4 rounded-md">
+          <h2 className="text-xl font-bold text-slate-600">{title}</h2>
+
+          <p className="text-slate-600">{description}</p>
+        </div>
+      </div>
     </div>
   );
 }
