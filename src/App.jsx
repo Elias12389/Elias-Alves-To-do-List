ebbe81f3498ef9182954014178065646135e6746
Elias12389 <eliasaslveees@gmail.com>
2026-08-06 08:56:55 -0300

    LocalStorage(feito)

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,17 @@
 import AddTask from "./components/AddTask";
 import Task from "./components/Task";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { v4 } from "uuid";
 
 function App() {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      title: "Estudar programação",
-      description: "Estudar programação para se tornar um dev full stack",
-      isCompleted: false,
-    },
-    {
-      id: 2,
-      title: "Estudar inglês",
-      description: "Estudar inglês para se tornar fluente.",
-      isCompleted: false,
-    },
-    {
-      id: 3,
-      title: "Estudar matemática",
-      description: "Estudar matemática para se tornar um dev full stack",
-      isCompleted: false,
-    },
-  ]);
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem("tasks")) || [],
+  );
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
 
   function onTaskClick(taskId) {
     const newTasks = tasks.map((task) => {
